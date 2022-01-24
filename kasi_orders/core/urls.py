7b5532993eeb7cbc08f2ab9f065bf2715f211a96
Luthando Zaze <lzaze@student.wethinkcode.co.za>
2022-01-24 13:08:08 +0200

    Some styling

--- kasi_orders/core/urls.py
+++ kasi_orders/core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import HomeView, RestaurantView
+from users import views as user_views
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home-page'),
-    path('restaurant/<int:pk>/', RestaurantView.as_view(), name='restaurant')
+    path('restaurant/<int:pk>/', RestaurantView.as_view(), name='restaurant'),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
--- kasi_orders/kasi_orders/settings.py
+++ kasi_orders/kasi_orders/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     # 'pwa'
 ]
 
@@ -119,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "core/static"
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
